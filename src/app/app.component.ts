92ce190c21ed87a4d3080f63e5dd1aa9925c23ba
@Ash9311 <ashutoshmulki7@gmail.com>
2022-08-08 01:26:25 +0530

    directives and some practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CRM';
+  success_msg=true;    
+  updated_msg=true;
+  error_msg=false;
+
+  contacts=[
+    {id:1,name:'John',age:23},
+    {id:2,name:'Smith',age:25},
+    {id:3,name:'Peter',age:30},
+    {id:4,name:'Jones',age:27},
+    {id:5,name:'Brown',age:28},
+  ]
+
+  
 }
